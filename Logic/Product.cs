b3c815cf636f553fbda51d0aac17f3cbfa7cae77
Adam Brodin <worecraft@gmail.com>
2020-11-08 19:59:11 +0100

    Project Setup

---
+++ Logic/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    class Product
+    {
+        public string title, description, imagePath;
+        public double price;
+
+        public Product(string title, string description, string imagePath, double price)
+        {
+            this.title = title;
+            this.description = description;
+            this.imagePath = imagePath;
+            this.price = price;
+        }
+    }
+}
---
+++ Logic/Store.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Logic
+{
+    public class Product
+    {
+        public string title, description, imagePath;
+        public double price;
+    }
+
+    public class Store
+    {
+        public static void Main()
+        {
+
+        }
+
+        public void ShowCart()
+        {
+
+        }
+
+        public void ApplyCoupon(String couponName)
+        {
+
+        }
+
+        public String GenerateReceipt()
+        {
+            return null;
+        }
+
+        public void Checkout()
+        {
+
+        }
+    }
+}
