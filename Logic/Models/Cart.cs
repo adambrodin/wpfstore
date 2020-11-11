89f18294ef9baebc87a8af383707caca122816c5
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-11-10 16:38:23 -0800

    Added more models

---
+++ Logic/Models/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Models
+{
+    public class Cart
+    {
+        public List<Product> products;
+    }
+}
---
+++ Logic/Models/Coupon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Models
+{
+    public class Coupon
+    {
+        public string code;
+        public double discount;
+        public bool precent;
+    }
+}
